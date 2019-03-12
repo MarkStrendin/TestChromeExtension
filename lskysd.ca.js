5ab8c8eacaf2735703a49ffb83f04bf94a192c38
Mark Strendin <mark.strendin@lskysd.ca>
2019-03-12 11:23:59 -0600

    Improved the content it's injecting so its not so disruptive

--- lskysd.ca.js
+++ lskysd.ca.js
@@ -1,1 +1,3 @@
-alert("HI!");
+$(function() {
+	$("body").before("<div style=\"width: 100%;background-color: yellow; padding: 5px; text-align: center;\">This banner was inserted by the chrome addon</div>");
+});
